f839ffaf64544d39f9a17905547875f75a8855c8
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-11-04 20:14:23 +0530

    Update App.jsx

--- React Practice/vitereactbasics/src/App.jsx
+++ React Practice/vitereactbasics/src/App.jsx
@@ -1,12 +1,17 @@
+import Faiz from "./faiz.jsx";
 
 
 function App() {
   
+
   return (
-    <h1>Hello, VITE the Beast is Here :) | Faizul Islam</h1>
+    // <h1>Hello, VITE the Beast is Here :) | Faizul Islam</h1>
+    <> 
+       <Faiz/>
+       <h1>Test kar Raha hun</h1>
+       <p>Aur Sab Kuch Samajh Aa raha hai</p>
+    </>
   )
 }
-
-
 
 export default App
