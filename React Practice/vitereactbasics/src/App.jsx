703dfc0e0dc8fa2a9865e92dfe9954613c56ada0
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-11-20 21:24:00 +0530

    Update App.jsx

--- React Practice/vitereactbasics/src/App.jsx
+++ React Practice/vitereactbasics/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     // <h1>Hello, VITE the Beast is Here :) | Faizul Islam</h1>
     <> 
        <Faiz/>
+      <h1>Previously i thought that react is hard, but i was wrong!! Its not that hard</h1>
        <h1>Test kar Raha hun</h1>
        <p>Aur Sab Kuch Samajh Aa raha hai</p>
     </>
